1ec7491e6fa82fc405b23753557b8569c2a79c2d
Priyankateke <priiyankateke1705@gmail.com>
2020-04-02 02:10:57 -0700

    ADDED: Welcome Message

---
+++ src/main/java/com/bankingsimulation/BankSimulation.java
@@ -0,0 +1,7 @@
+package com.bankingsimulation;
+
+public class BankSimulation {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Banking Cash Counter Simulation");
+    }
+}
